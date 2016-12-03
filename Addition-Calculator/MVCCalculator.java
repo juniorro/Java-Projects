921d6ce76d5544a432a4d02330d454236e98132e
Roland Junior Toussaint <juniorro98@outlook.com>
2016-12-03 10:23:27 -0500

    Added more comments for better code understanding

--- Addition-Calculator/MVCCalculator.java
+++ Addition-Calculator/MVCCalculator.java
@@ -1,4 +1,12 @@
-
+/**
+* The MVCCalculator program implements Model-View-Controller model
+* application that simply adds two numbers that the user enters
+* using Swing GUI ad displays the result
+*
+* @author  Roland Junior Toussaint
+* @version 1.0
+* @since   10/03/2016
+*/
 public class MVCCalculator {
 	
 	public static void main (String[] args){
