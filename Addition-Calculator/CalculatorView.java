b9ebb2e68155367ff09f68178622d128a897185d
Roland Junior Toussaint <juniorro98@outlook.com>
2016-12-03 10:22:57 -0500

    Added more comments for better code understanding

--- Addition-Calculator/CalculatorView.java
+++ Addition-Calculator/CalculatorView.java
@@ -1,9 +1,24 @@
+/**
+* The Game program implements an application that
+* simply asks a user to guessed a random number generated
+* by the computer. It will run until the
+* user guesses the correct number.* 
+*
+* @author  Roland Junior Toussaint
+* @version 1.0
+* @since   10/03/2016
+*/
+
+//import acion listerner
 import java.awt.event.ActionListener;
+
+//import Swing library
 import javax.swing.*;
 
 
 public class CalculatorView extends JFrame {
 
+		//GUI controllers
 		private JTextField NumberOne = new JTextField(10);
 		private JLabel additonSign = new JLabel("+");
 		private JTextField NumberTwo = new JTextField(10);
@@ -32,14 +47,32 @@ public class CalculatorView extends JFrame {
 		}
 
 		public int getNumberOne(){
+			 /**
+			   * This method is used to get the first number
+			   * parsed into the number GUI
+			   * @param none
+			   * @return int number 
+			   */	
 			return Integer.parseInt(NumberOne.getText());
 		}
 		
 		public int getNumberTwo(){
+			/**
+			   * This method is used to get the second number
+			   * parsed into the number GUI
+			   * @param none
+			   * @return int number 
+			   */
 			return Integer.parseInt(NumberTwo.getText());
 		}
 		
 		public int getAnswer(){
+			/**
+			   * This method is used to get the answer
+			   * parsed into the number GUI
+			   * @param none
+			   * @return int answer 
+			   */
 			return Integer.parseInt(Answer.getText());
 		}
 		
@@ -53,7 +86,14 @@ public class CalculatorView extends JFrame {
 		}
 		
 		void invalidEntry (String errorMessage){
+			/**
+			   * This method displays an error message
+			   * if a string is entered.
+			   * @param none
+			   * @return int answer 
+			   */
 			
+			//show message
 			JOptionPane.showMessageDialog(this, errorMessage);
 		}
 }
