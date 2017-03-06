d3b2fd5f4ac69bf1f1e7719ef65b0e439fd71f1d
Roland Junior Toussaint <juniorro@juniorro-pc.attlocal.net>
2017-03-06 05:19:58 +0000

    Remove duplicate repo

--- BANKSYSTEM/src/main/java/com/main/repositories/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.main.entities.Account;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-
-}
