d43d73efc357ec64f1b5dedd186b7071d3e9079d
juniorro <juniorro98@outlook.com>
2017-03-26 02:13:15 -0400

    Fixed time format

--- AccountManager/src/main/java/app/security/LoginController.java
+++ AccountManager/src/main/java/app/security/LoginController.java
@@ -1,18 +1,18 @@
-package app.security;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class LoginController {
-		
-		@RequestMapping(value="/login")
-		public String login(){			
-			//return "login";
-			return "login";
-		}
-		
-		@RequestMapping(value="/home")
-		public String home(){			
-			return "homepage";
-		}
-}
+//package app.security;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class LoginController {
+//		
+////		@RequestMapping(value="/login")
+////		public String login(){			
+////			//return "login";
+////			return "login";
+////		}
+//		
+//		@RequestMapping(value="/home")
+//		public String home(){			
+//			return "homepage";
+//		}
+//}
