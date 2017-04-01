890003d9ec781b0d8dc9ec6d8b976ed50a959cde
juniorro <juniorro98@outlook.com>
2017-03-31 22:30:12 -0400

    Renamed repo

--- Person/src/main/java/model/dao/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package model.dao;
-import org.springframework.data.repository.CrudRepository;
-import model.Person;
-
-public interface PersonRepository extends CrudRepository<Person, Integer>{
-
-}
